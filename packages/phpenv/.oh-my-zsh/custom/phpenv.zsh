349b70d16ac6fbce723e946f251d35560f02d18f
Cameron Messinides <cameron.messinides@gmail.com>
2024-02-26 18:59:12 -0500

    Split remaining feature-specific ZSH files into packages

--- packages/phpenv/.oh-my-zsh/custom/phpenv.zsh
+++ packages/phpenv/.oh-my-zsh/custom/phpenv.zsh
@@ -1,4 +1,4 @@
-export PHPENV_ROOT="/home/cmessinides/.phpenv"
+export PHPENV_ROOT="$HOME/.phpenv"
 if [ -d "${PHPENV_ROOT}" ]; then
   export PATH="${PHPENV_ROOT}/bin:${PATH}"
   eval "$(phpenv init -)"
