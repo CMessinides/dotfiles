61a6dbe00308b687ce1ece0f907a8ccdf2aac315
Cameron Messinides <cameron.messinides@gmail.com>
2025-01-07 11:47:41 -0500

    Add <Ctrl-F> project launcher to fish

---
+++ packages/fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+# Set fish as the default shell for things like tmux
+set -gx SHELL "$(command -s fish)"
+
+# Set editor to the installed flavor of vim
+if type -q nvim
+    set -gx EDITOR nvim
+else
+    set -gx EDITOR vim
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # <Ctrl-F> to open a project launcher
+    bind \cf 'open-project; commandline -f repaint'
+end
---
+++ packages/fish/.config/fish/functions/open-project.fish
@@ -0,0 +1,25 @@
+function open-project -d 'Open a project in tmux'
+    set -l project_root
+    if set -q PROJECT_ROOT
+        set project_root "$PROJECT_ROOT"
+    else
+        set project_root "$HOME/source"
+    end
+
+    set -l project "$(find "$project_root"/* -maxdepth 0 -type d -exec basename {} \; | fzf)"
+
+    if [ -n "$project" ]
+        # Test for existing session
+        tmux has-session -t "$project" 2>/dev/null
+
+        if [ $last_pid != 0 ]
+            tmux new-session -d -c "$project_root/$project" -s "$project"
+        end
+
+        if [ -n "$TMUX" ]
+            tmux switch-client -t "$project"
+        else
+            tmux attach-session -t "$project"
+        end
+    end
+end
