f9c17646e96f0733d5722c401b8df2365432754d
Cameron Messinides <cameron.messinides@gmail.com>
2025-01-08 17:25:04 -0500

    Add capture template for stories

--- packages/org/.doom.d/custom/org.el
+++ packages/org/.doom.d/custom/org.el
@@ -8,7 +8,14 @@
                  :clock-in t
                  :clock-resume t
                  :empty-lines 1))
-
+  (add-to-list 'org-capture-templates
+               '("s" "Story"
+                 entry (file+headline "work.org" "Uncategorized")
+                 "* TODO %?\n\n [[https://app.clickup.com/t/%^{Task ID}][Task %\\1]]"
+                 :clock-in t
+                 :clock-keep t
+                 :jump-to-captured t
+                 :empty-lines 1))
   (add-to-list 'org-capture-templates
                '("c" "Call"
                  entry (file+headline +org-capture-todo-file "Calls")
