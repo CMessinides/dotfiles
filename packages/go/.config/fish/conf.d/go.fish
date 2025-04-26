d56f1dcb449c4fc413d219e1b4a2327ebe942c26
Cameron Messinides <cameron.messinides@gmail.com>
2025-04-26 10:15:26 -0400

    fix(go): add $GOPATH/bin to PATH

--- packages/go/.config/fish/conf.d/go.fish
+++ packages/go/.config/fish/conf.d/go.fish
@@ -1,5 +1,8 @@
 set GO_INSTALL "/usr/local/go"
+set GOPATH "$HOME/go"
 
 if [ -d "$GO_INSTALL" ]
     fish_add_path "$GO_INSTALL/bin"
+    fish_add_path "$GOPATH/bin"
+    fish_add_path "$(go env GOPATH)/bin"
 end
