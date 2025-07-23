46310094bf949963eda4bd32090bfda415a99d44
Cameron Messinides <cameron.messinides@gmail.com>
2025-07-23 13:42:23 -0400

    fix(go): ensure tools added with "go install" are in PATH

--- packages/go/.config/fish/conf.d/go.fish
+++ packages/go/.config/fish/conf.d/go.fish
@@ -1,8 +1,9 @@
 set GO_INSTALL "/usr/local/go"
-set GOPATH "$HOME/go"
-
 if [ -d "$GO_INSTALL" ]
     fish_add_path "$GO_INSTALL/bin"
-    fish_add_path "$GOPATH/bin"
+end
+
+set -gx GOPATH "$HOME/go"
+if command -q go
     fish_add_path "$(go env GOPATH)/bin"
 end
