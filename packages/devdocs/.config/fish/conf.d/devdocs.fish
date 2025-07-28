e843b8bd7a4fc3ce99d9979ab19cdb0ca9dee4e9
Cameron Messinides <cameron.messinides@gmail.com>
2025-07-28 01:13:36 -0400

    feat(devdocs): add shell and tmux integrations

--- packages/devdocs/.config/fish/conf.d/devdocs.fish
+++ packages/devdocs/.config/fish/conf.d/devdocs.fish
@@ -5,3 +5,20 @@ if command -q bat
         set -gx DEVDOCS_PAGER "bat"
     end
 end
+
+if status is-interactive
+    # Search DevDocs documentation
+    bind ctrl-s,ctrl-s 'docsearch; commandline -f repaint'
+
+    # Shortcuts for frequently referenced docs
+    # Go
+    bind ctrl-s,ctrl-g 'docsearch go; commandline -f repaint'
+    # CSS
+    bind ctrl-s,ctrl-c 'docsearch css; commandline -f repaint'
+    # HTML
+    bind ctrl-s,ctrl-h 'docsearch html; commandline -f repaint'
+    # JavaScript
+    bind ctrl-s,ctrl-j 'docsearch javascript; commandline -f repaint'
+    # Node.js
+    bind ctrl-s,ctrl-n 'docsearch node; commandline -f repaint'
+end
