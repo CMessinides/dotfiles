091abcc51bf2be338469c87cfd348b184b96c095
Cameron Messinides <cameron.messinides@gmail.com>
2025-11-04 10:51:47 -0500

    feat(nvim): add keybinding for searching files in cwd

--- packages/nvim/.config/nvim/init.lua
+++ packages/nvim/.config/nvim/init.lua
@@ -674,6 +674,11 @@ vim.keymap.set("n", "<leader>sd", function()
         find_command = { "fd", "-t", "d" },
     })
 end, { desc = "[S]earch [D]irectories" })
+vim.keymap.set("n", "<leader>s.", function()
+    require("telescope.builtin").find_files({
+        cwd = require("telescope.utils").buffer_dir(),
+    })
+end, { desc = "[S]earch This Directory" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
