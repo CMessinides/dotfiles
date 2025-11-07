68494475caedd968cd4550db415efbff32d2f8fe
Cameron Messinides <cameron.messinides@gmail.com>
2025-11-07 16:20:23 -0500

    feat(fish): tweak UI for project picker

--- packages/fish/.config/fish/functions/pick-project.fish
+++ packages/fish/.config/fish/functions/pick-project.fish
@@ -1,3 +1,6 @@
 function pick-project -d "Pick a project with fzf"
-    find "$PROJECT_ROOT"/* -maxdepth 0 -type d -exec basename {} \; | fzf
+    find "$PROJECT_ROOT"/* -maxdepth 0 -type d -exec basename {} \; | fzf \
+        --border-label " Projects " \
+        --border-label-pos 2 \
+        --height '~25%'
 end
