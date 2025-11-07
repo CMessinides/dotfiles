4b5624d9f9a0df0b113255e1a9a590a512a93dce
Cameron Messinides <cameron.messinides@gmail.com>
2025-11-07 14:37:05 -0500

    feat(fzf): override default fzf appearance

--- packages/fzf/.config/fish/conf.d/fzf.fish
+++ packages/fzf/.config/fish/conf.d/fzf.fish
@@ -1,2 +1,18 @@
 # Set up fzf key bindings
 fzf --fish | source
+
+# Prettify the default fzf
+set -gx FZF_DEFAULT_OPTS "
+    --border horizontal --reverse --cycle
+    --preview-window 'right,border-left,<66(down,50%,border-top)'
+    --color 16
+    --color border:grey
+    --color prompt:blue
+    --color pointer:blue
+    --color info:italic:blue
+    --color label:italic:blue
+    --color fg+:bold:bright-magenta,bg+:-1
+    --color hl:underline:yellow,hl+:bold:underline:bright-yellow
+    --prompt '❱ '
+    --pointer '▒'
+"
